240932d9b2d3298fd0ee8077559f5c6af02c5112
Alejandro Quisbert Carvajal <alexandro@autistici.org>
2018-01-16 17:33:41 -0400

    Modelo Carreras creado

--- apps/afiliando/admin.py
+++ apps/afiliando/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from apps.afiliando.models import Estudiante
+from apps.afiliando.models import Estudiante, Carrera
 
 admin.site.register(Estudiante)
+admin.site.register(Carrera)
--- apps/afiliando/models.py
+++ apps/afiliando/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=30)
+    sigla = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.sigla
+
+
 class Estudiante(models.Model):
     ci = models.CharField(primary_key=True, max_length=20, null=False)
     nombres = models.CharField(max_length=30)
     apellido_paterno = models.CharField(max_length=30)
     apellido_materno = models.CharField(max_length=30)
     matricula = models.PositiveIntegerField()
-    carrera = models.CharField(max_length=30)
+    carrera = models.ManyToManyField(Carrera)
     fecha_de_nacimento = models.DateField()
     direccion = models.CharField(max_length=50)
 
